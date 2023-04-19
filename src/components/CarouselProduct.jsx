4cf52ea747c8c6abb65590425ff30f3493d59a6f
Mark Behnke <mark.behnke1998@gmail.com>
2023-04-18 18:23:48 -0700

    Added the homepage

---
+++ src/components/CarouselProduct.jsx
@@ -0,0 +1,22 @@
+import {Swiper, SwiperSlide} from 'swiper/react'
+import "swiper/css";
+import "swiper/css/navigation"
+import { Navigation } from 'swiper'
+
+
+const CarouselProduct = () => {
+  return (
+    <div className="bg-white m-3">
+        <div className='text-2xl font-semibold p-3'>Best Sellers</div>
+        <Swiper slidesPerView={7} spaceBetween={10} navigation={true} modules={[Navigation]}> 
+        {
+        Array.from({length: 9}, (_, i) =>
+            <SwiperSlide key={i}>
+                <img src={`../images/product_${i}_small.jpg`}/>
+            </SwiperSlide>)
+        }
+    </Swiper></div>
+  )
+}
+
+export default CarouselProduct
